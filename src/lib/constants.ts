5c852c0a6b56aef7dd12e03b9cee2b21ac4e8e65
Lucca <luccapavanallo@exemplo.com>
2026-03-13 17:13:30 -0300

    feat: adicionar responsáveis Stella, Rogério, Thais, Gregório e Sueli

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,3 @@
 export const ADMIN_EMAIL = 'luccapavanallo@gmail.com';
 
-export const RESPONSAVEIS = ['Sombrear', 'Teste'];
+export const RESPONSAVEIS = ['Sombrear', 'Teste', 'Stella', 'Rogério', 'Thais', 'Gregório', 'Sueli'];
